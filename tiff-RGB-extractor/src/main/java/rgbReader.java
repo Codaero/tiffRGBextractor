65eaaf61a7d18544f209f9075575adfe1fec6408
EpicMisterB <45112325+EpicMisterB@users.noreply.github.com>
2020-05-04 03:24:54 -0500

    initial commit set up maven dependency for the .tiff reader library

---
+++ tiff-RGB-extractor/src/main/java/rgbReader.java
@@ -0,0 +1,21 @@
+import mil.nga.tiff.*;
+import java.io.File;
+
+public class rgbReader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Extracts RGB values from a .tiff file
+	 * 
+	 * @return Returns a 3D array [height][width][r,g,b]
+	 */
+	public int[][][] extractRGB(File file)
+	{
+		
+	}
+	
+}
